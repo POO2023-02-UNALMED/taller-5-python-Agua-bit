19fa89526c052b446e0c0f3d507e7ffee2b087b2
= <60821161+Agua-bit@users.noreply.github.com>
2023-09-25 20:21:48 -0500

    vgh

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,6 +1,6 @@
 class Zona:
 
-    def __init__(self, nombre, zoo):
+    def __init__(self, nombre, zoo = None):
         self._animales = []
         self._nombre = nombre
         self._zoo = zoo
@@ -17,3 +17,9 @@ class Zona:
     
     def setNombre(self, _nombre):
         self._nombre = _nombre
+
+    def getZoo(self):
+        return self._zoo
+    
+    def setZoo(self, _zoo):
+        self._zoo = _zoo
