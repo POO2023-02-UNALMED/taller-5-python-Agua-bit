5ba148f7d3927901bd018423ad21ce9013e96464
= <60821161+Agua-bit@users.noreply.github.com>
2023-09-25 22:41:05 -0500

    c

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -16,7 +16,7 @@ class Animal:
     
     @staticmethod
     def totalPorTipo():
-        return "Mamiferos: " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos) + "\n" "Aves : " + str(zooAnimales.ave.Ave.cantidadAves) + "\n" "Reptiles: " + str(zooAnimales.reptil.Reptiles.cantidadReptiles) + "\n" "Peces: " + str(zooAnimales.pez.Pez.cantidadPeces) + "\n" "Anfibios: " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios)
+        return "Mamiferos: " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos) + "\n" "Aves : " + str(zooAnimales.ave.Ave.cantidadAves) + "\n" "Reptiles: " + str(zooAnimales.reptil.Reptil.cantidadReptiles) + "\n" "Peces: " + str(zooAnimales.pez.Pez.cantidadPeces) + "\n" "Anfibios: " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios)
     
     def toString(self):
         return f"Mi nombre es {self._nombre}, tengo una edad de {self._edad}, habito en {self._habitat} y mi genero es {self._genero}, la zona en la que me ubico es {self._zona.getNombre()}, en el {self._zona.getZoo().getNombre()}"
